9296e40588984e4076f18e49e079643f2e6ca28b
unknown <fozlurrahman9933@gmail.com>
2023-09-08 12:46:19 +0600

    knapsack of dynamic programming

---
+++ knapsack-iteration.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int v[n], w[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> w[i];
+    }
+    int s;
+    cin >> s;
+
+    return 0;
+}
